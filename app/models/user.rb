68cf11bc300f49072164a54a63cdf05de821780e
scrapcode <codectc@gmail.com>
2014-04-22 00:29:23 -0500

    Fix issue #1

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, on: :create
   
   has_many :marks
   
