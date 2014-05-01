60badcee429d8e0f4cdbdd94b1d444aaab37b8aa
scrapcode <codectc@gmail.com>
2014-05-01 01:18:56 +0000

    Redirect to root if signed in and tries accesing session pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,16 +8,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_back_or root_path
+      flash[:notice] = "Successful sign in."
+      redirect_back_or user
     else
-      flash.now[:error] = 'Invalid email/pass combination. Please try again...'
+      flash.now[:error] = 'Invalid email/pass combination'
       render 'new'
     end
   end
 
   def destroy
     sign_out
-    redirect_to root_path
+    redirect_to root_url
   end
   
   private
